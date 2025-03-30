aed8a9fbe42aeadec762d928f89ef4a470947e5b
gayathri-srinii <gayathrisrinivasan.2906@gmail.com>
2025-03-30 19:38:38 +0530

    update

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/')
 def home():
@@ -9,3 +11,12 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/api', methods=['GET', 'POST'])
+def api():
+    if request.method == 'GET':
+        return jsonify({"message": "This is a GET request"})
+    elif request.method == 'POST':
+        data = request.json
+        print(data)  # Parse JSON data from the request body
+        return jsonify({"message": "This is a POST request", "data": data})
